3a2a0f71629d9e98bb2d14a46b89bed5c6b8c9f4
Irfan Rasheed <irfanrasheedadv@gmail.com>
2023-02-22 03:51:23 +0500

    next13-blog

---
+++ app/(marketing)/posts/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Posts() {
+  const router = useRouter();
+  return (
+    <main className={styles.main}>
+  
+      <div className={styles.center}><h1> Post!</h1> </div>
+      <Link href='/'>back to hom</Link>
+      <button
+        type="button" onClick={() => router.push('/')}
+      >Back to Home!</button>
+    </main>
+  );
+}
---
+++ data/posts.ts
@@ -0,0 +1,5 @@
+export const posts = [
+    {id:"1", title:"Welcome to Portfolio Page.", date:"2020-01-02"},
+
+    {id:"2", title:"Get All Information About me", date:"2020-01-02"}
+];
